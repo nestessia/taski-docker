e1f4e989991f95421eb174faa8fbf49e14d62c22
nestessia <nastya_kriychkova@bk.ru>
2023-11-13 23:56:07 +0300

    tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.9.52', '127.0.0.1', 'localhost', 'taski-nestesia.ddns.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,6 +122,3 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
-]
